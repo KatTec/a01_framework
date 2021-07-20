e6cc376d8a8810e7946bc0b17a9d480b37073a14
TomKat_Ivyb-pc <tomisitec102@gmail.com>
2021-07-20 21:37:00 +0900

    layout, header, footer追加

---
+++ components/layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout(props) {
+  return (
+    <div>
+      <div>-layout.js-</div>
+      <Head>
+        <title>{props.title}</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous"></link>
+      </Head>
+
+      <Header header={props.header} />
+
+      <div className="container">
+        <h3 className="my-3 text-primary text-center"> {props.title}</h3>
+        <div>props.childrenとしてindex.jsのreturnを返す</div>
+        {props.children}
+      </div>
+
+      <Footer footer="copyright SYODA-Tuyano." />
+
+    </div>
+  )
+}
