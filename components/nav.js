826651b1a5450cd9fb775df37242c8507ccc8636
TomKat_Ivyb-pc <tomisitec102@gmail.com>
2021-07-26 20:54:47 +0900

    nav.js：navタグ追加

--- components/nav.js
+++ components/nav.js
@@ -8,6 +8,18 @@ export default function nav(props){
                 <li className="nav-item"><a className="nav-link" href="#" disable>c無効</a></li>
             </ul>
 
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="display-3">Sample.</h2>
+                    <p className="lead">this is navigation sample page.
+                        this is navigation sample page.
+                        this is navigation sample page.
+                        this is navigation sample page.<br/>
+                        this is navigation sample page.
+                    </p>
+                </div>
+            </div>
+
         </div>
     )
 }
