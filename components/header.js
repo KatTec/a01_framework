f3dd77089173ba6fd8fb8092bcb3525fb0a9bcfe
TomKat_Ivyb-pc <tomisitec102@gmail.com>
2021-07-28 12:18:25 +0900

    index, page2_hello画面のLinkタグ作成

--- components/header.js
+++ components/header.js
@@ -1,8 +1,38 @@
+import Link from 'next/link'
+
 export default function Header(props) {
   return (
-    <div>
-      <h1 className="bg-primary px-3 text-white display-4 text-right">
-        {props.header}</h1>
+    <div className="container">
+      <h4>-header.js-</h4>
+      <h1 className="bg-primary px-3 text-white display-6 text-right">
+        {props.header}
+      </h1>
+
+      <div className="container ">
+        <div className="bg-info text-secondary">
+        </div>
+
+        <ul className="nav">
+          <li className="nav-item nav-link bg-primary">
+            {/* <a className="nav-link bg-primary" href="/">index</a> */}
+            <Link href="/"> 
+                <a>indexはこちら</a>
+            </Link>
+          </li>
+
+          <li className="nav-item nav-link bg-secondary">
+            {/* <a className="nav-link bg-secondary" href="/page2/page2_hello" active>page2_hello</a> */}
+            <Link href="/page2/page2_hello"> 
+              <a>page2_helloはこちら</a>
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link bg-warning" href="#" disable>c無効</a>
+          </li>
+        </ul>
+
+      </div>
     </div>
   )
 }
