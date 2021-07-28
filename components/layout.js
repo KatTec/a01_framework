92ccc30e1b07dd581fec354cee0d6141d60cc72a
TomKat_Ivyb-pc <tomisitec102@gmail.com>
2021-07-28 12:17:43 +0900

    layputから各種コンテンツを削除。index.jsに登録&Bootstrap呼び出し

--- components/layout.js
+++ components/layout.js
@@ -1,37 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Header from './Header'
-import Footer from './Footer'
-import Table from './table'
-import Nav from './nav'
-import Col from './col'
+import Header from './header'
+import Footer from './footer'
 
 export default function Layout(props) {
   return (
-    <div>
+    <div className="container bg-dark">
       <h3>-layout.js-</h3>
       <Head>
         <title>{props.title}</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous"></link>
+        {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous"></link> */}
       </Head>
 
-      <Nav></Nav>
+      <Header header={props.header}/>
 
-      <Header header={props.header} />
-
-      <Col></Col>
       <hr/>
-
-      <Table></Table>
-
       <div className="container">
         <h3 className="my-3 text-primary text-center"> {props.title}</h3>
         <div>以下に、props.childrenとしてindex.jsのreturnを返す</div>
         {props.children}
       </div>
+      <hr/>
 
-      <Footer footer="copyright SYODA-Tuyano." />
-
+      <Footer/>
     </div>
   )
 }
